68a863d418f4ba7164db72df3bebf9ada1ec8bef
v_prytamannyi <pritamanny255@gmail.com>
2024-08-20 17:16:24 +0300

    Changed seeding migration

--- src/Infrastructure/Identity/Migrations/20240721112558_Seeding.cs
+++ src/Infrastructure/Identity/Migrations/20240721112558_Seeding.cs
@@ -24,7 +24,8 @@ namespace Infrastructure.Migrations
                 columns: new[] { "Id", "UserName", "NormalizedUserName", "Email", "NormalizedEmail", "EmailConfirmed", "PasswordHash", "SecurityStamp", "ConcurrencyStamp", "PhoneNumber", "PhoneNumberConfirmed", "TwoFactorEnabled", "LockoutEnd", "LockoutEnabled", "AccessFailedCount" },
                 values: new object[,]
                 {
-                    { "31d68aca-74e6-4714-ae67-bcfa7eb347af", "clothingshop335@gmail.com", "CLOTHINGSHOP335@GMAIL.COM", "clothingshop335@gmail.com", "CLOTHINGSHOP335@GMAIL.COM", true, "AQAAAAIAAYagAAAAEPMZ0QofQpKGjWTMlAv06c6Iq52iYWW1xncrlj5ds6LMziw5bSnt2EIWeHDIS+drEg==", "S5MWCC4T5TGKHSNOTNKBL5J7OFA5SYSL", "3638e981-8e63-45ab-85d0-bb159299ae34", null, false, false, null, true, 0 }
+                    { "31d68aca-74e6-4714-ae67-bcfa7eb347af", "clothingshop335@gmail.com", "CLOTHINGSHOP335@GMAIL.COM", "clothingshop335@gmail.com", "CLOTHINGSHOP335@GMAIL.COM", true, "AQAAAAIAAYagAAAAEPMZ0QofQpKGjWTMlAv06c6Iq52iYWW1xncrlj5ds6LMziw5bSnt2EIWeHDIS+drEg==", "S5MWCC4T5TGKHSNOTNKBL5J7OFA5SYSL", "3638e981-8e63-45ab-85d0-bb159299ae34", null, false, false, null, true, 0 },
+                    { "0223a7f2-91e7-44d1-9684-7c292dd6061d", "pritamanny255@gmail.com", "PRITAMANNY255@GMAIL.COM", "pritamanny255@gmail.com", "PRITAMANNY255@GMAIL.COM", true, "AQAAAAIAAYagAAAAEGwbuRs7HCNhJKt4EGBrrPK3A13nfDFQi/ph280g20FBQyzt/9QCBjOPrWitWmjnBA==", "7NLUXKZLLS5C5R37DUVK54N6QTFTEOS6", "c7e05893-c88b-4b7e-9b90-d0a92cab387f", null, false, false, null, true, 0 }
                 });
 
             migrationBuilder.InsertData(
@@ -32,7 +33,8 @@ namespace Infrastructure.Migrations
                 columns: new[] { "Id", "UserId", "ClaimType", "ClaimValue" },
                 values: new object[,]
                 {
-                    { 1, "31d68aca-74e6-4714-ae67-bcfa7eb347af", "Role", "Admin" }
+                    { 1, "31d68aca-74e6-4714-ae67-bcfa7eb347af", "Role", "Admin" },
+                    { 2, "0223a7f2-91e7-44d1-9684-7c292dd6061d", "Role", "User" }
                 });
 
             migrationBuilder.InsertData(
@@ -40,7 +42,8 @@ namespace Infrastructure.Migrations
                 columns: new[] { "UserId", "RoleId" },
                 values: new object[,]
                 {
-                    { "31d68aca-74e6-4714-ae67-bcfa7eb347af", "79f392bf-21e3-4e59-a899-f2a066f3884a" }
+                    { "31d68aca-74e6-4714-ae67-bcfa7eb347af", "79f392bf-21e3-4e59-a899-f2a066f3884a" },
+                    { "0223a7f2-91e7-44d1-9684-7c292dd6061d", "5849bcbd-fa3c-46ad-8ac1-00ff8cd9a6d7" }
                 });
 
             migrationBuilder.InsertData(
